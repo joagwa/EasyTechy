2a92f6e37509bbf9e1702a7900f40bad8b633980
aeronan <btyegj@gmail.com>
2015-11-04 17:26:14 +1000

    add more items

--- Project/EasyCheckOut/EasyCheckOut/EasyCheckOut.cs
+++ Project/EasyCheckOut/EasyCheckOut/EasyCheckOut.cs
@@ -49,9 +49,15 @@ namespace EasyCheckOut
 				WoolworthsItem item1 = new WoolworthsItem ("50375264", "Kleenex Tissues", 2.50, prefix + "tissue.png");
 				WoolworthsItem item2 = new WoolworthsItem ("4001686301562", "Haribo Goldbears", 1.35, prefix + "Haribo-Gold-Bears.png");
 				WoolworthsItem item3 = new WoolworthsItem ("4947678649536", "Body Sheet", 2.80, prefix + "bodysheet.png");
+				WoolworthsItem item4 = new WoolworthsItem ("9310055101432", "Crunchy Nut", 7.60, prefix + "crunchy_nut.jpg");
+				WoolworthsItem item5 = new WoolworthsItem ("9310055537286", "Kellogg's Original", 8.20, prefix + "Kelloggs-Original.png");
+				WoolworthsItem item6 = new WoolworthsItem ("9300675011037", "Coke Cola Diet", 3.20, prefix + "cocacola-diet.png");
 				database.InsertItemToWoolWorthsItem (item1);
 				database.InsertItemToWoolWorthsItem (item2);
 				database.InsertItemToWoolWorthsItem (item3);
+				database.InsertItemToWoolWorthsItem (item4);
+				database.InsertItemToWoolWorthsItem (item5);
+				database.InsertItemToWoolWorthsItem (item6);
 			}
 		}
 
